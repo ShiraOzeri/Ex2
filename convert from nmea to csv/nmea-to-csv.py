0cb5476c9e0b5a1118c85e9c1a6c63f3a676c7fa
shaharelbaz <shahar_112@walla.com>
2016-04-22 17:59:48 +0300

    Added files via upload

--- convert from nmea to csv/nmea-to-csv.py
+++ convert from nmea to csv/nmea-to-csv.py
@@ -2,6 +2,7 @@ import csv
 from datetime import datetime
 import math
 import sys
+import time
 
 # adapt this to your file
 INPUT_FILENAME = sys.argv[1]
@@ -24,6 +25,8 @@ with open(INPUT_FILENAME, 'r') as input_file:
         writer.writerow(['date','time','speed', 'latitude','latitude direction' ,'longtitude','longtitude direction', 'fix', 'horizontal', 'altitude','altitude direction'])
 
         # iterate over all the rows in the nmea file
+        date = None
+        time = None
         for row in reader:
 
             # skip all lines that do not start with $GPRMC
@@ -46,15 +49,21 @@ with open(INPUT_FILENAME, 'r') as input_file:
                 altitude=row[8]
                 altitude_direction=row[9]
 
+            if (date is not None and time is not None):
+                # merge the time and date columns into one Python datetime object (usually more convenient than having both separately)
 
                 # merge the time and date columns into one Python datetime object (usually more convenient than having both separately)
-                time= datetime.strptime(time, '%H%M%S.%f')
-                date= datetime.strptime(date, '%d%m%y')
+                #date = datetime.strptime(date, '%d%m%y')
 
                 # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
-                date = date.strftime('%d-%m-%y')[:-3] # [:-3] cuts off the last three characters (trailing zeros from the fractional seconds)
+                #date = date.strftime('%y-%m-%d')
+                #time = datetime.strptime(time, '%H%M%S.%f')
 
-                time = time.strftime('%H:%M:%S.%f')[:-3]
+                # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
+               # time = time.strftime('%H:%M:%S.%f')[       :-3]  # [:-3] cuts off the last three characters (trailing zeros from the fractional seconds)
+
+                # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
+
                 # lat and lon values in the $GPRMC nmea sentences come in an rather uncommon format. for convenience, convert them into the commonly used decimal degree format which most applications can read.
                 # the "high level" formula for conversion is: DDMM.MMMMM => DD + (YY.ZZZZ / 60), multiplicated with (-1) if direction is either South or West
                 # the following reflects this formula in mathematical terms.
@@ -74,5 +83,5 @@ with open(INPUT_FILENAME, 'r') as input_file:
                 speed = int(round(float(speed) * 1.852, 0))
 
                 # write the calculated/formatted values of the row that we just read into the csv file
-                writer.writerow([date,time,speed, latitude,latitude_direction,longtitude,longtitude_direction, fix, horizontal, altitude,altitude_direction])
+                writer.writerow([date , time,speed, latitude,latitude_direction,longtitude,longtitude_direction, fix, horizontal, altitude,altitude_direction])
 print "Done!"
